0ef28f5beecda5eee79a78a21ca9e692d50fbcf0
HanbitGaram <webmaster@hanb.jp>
2018-10-10 12:04:17 +0900

    연결문제 해결 위해 일부 소스 추가

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
