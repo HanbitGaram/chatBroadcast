ec788b35f9941d7bdf8f3237dac586c375ae7223
HanbitGaram <webmaster@hanb.jp>
2018-10-10 12:46:39 +0900

    모듈

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.use(function(req, res, next) {
 });
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var ios = socketIO(http);
 
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
